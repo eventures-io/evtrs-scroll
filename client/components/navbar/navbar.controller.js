71d768c2d76021560728ac6f34a15ba05df335fc
Peter Symoens <peter.symoens@eventures.io>
2014-11-01 21:01:51 +0100

    hide menu on home page

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,20 +1,34 @@
 'use strict';
 
 angular.module('evtrsScrollApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+    .controller('NavbarCtrl', function ($scope, $location, Auth, $rootScope) {
+
+        if($location.path() === '/') {
+            $scope.navbarHidden = 'navbar-hidden';
+        }
+
+        $rootScope.$on('$stateChangeSuccess',
+            function (event, toState, toParams, fromState, fromParams) {
+                if (toState.name === 'base.home.intro') {
+                    $scope.navbarHidden = 'navbar-hidden';
+                }
+                else {
+                    $scope.navbarHidden = '';
+                }
+            })
+
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.isAdmin = Auth.isAdmin;
+        $scope.getCurrentUser = Auth.getCurrentUser;
+
+        $scope.logout = function () {
+            Auth.logout();
+            $location.path('/login');
+        };
+
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        };
 
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
-
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
-
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+    });
