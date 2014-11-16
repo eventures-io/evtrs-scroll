4fd791beda08931d22ce6279954f1213a78c6931
Peter Symoens <peter.symoens@eventures.io>
2014-11-16 14:12:44 +0100

    update article controller tests

--- client/test/article/article.controller.spec.js
+++ client/test/article/article.controller.spec.js
@@ -1,82 +1,91 @@
 'use strict'
 describe('Controller : ArticleCtrl', function () {
 
-    var articleResourceStub, scope;
+    var articleResourceStub,
+        $q,
+        $rootScope,
+        $scope,
+        deferred;
 
     beforeEach(module('evtrsScrollApp'));
 
-    beforeEach(function () {
-
-        articleResourceStub = sinon.stub({'save': function () {
+    beforeEach(function() {
+        articleResourceStub = sinon.stub({'save': function (article) {
+            return deferred.promise;
         }});
-
         module(function ($provide) {
             $provide.value('ArticleResource', articleResourceStub);
         });
-    });
 
-    beforeEach(inject(function ($controller, $rootScope, $q) {
-        scope = $rootScope.$new();
-        $controller('ArticleCtrl', {$scope: scope});
+    })
+    beforeEach(inject(function (_$rootScope_, _$q_) {
+        $rootScope = _$rootScope_;
+        $q = _$q_;
+        deferred = $q.defer();
     }));
+
+    beforeEach(inject(function ($controller) {
+        $scope = $rootScope.$new();
+        $controller('ArticleCtrl', {'$scope': $scope});
+        articleResourceStub.save.returns(deferred.promise);
+
+    }));
+
+    describe('initialize', function () {
+        it('should initialize a new article and reset saveAction, submitted on the $scope', function () {
+            $scope.article = {title: 'testTitle'};
+            $scope.initialize();
+            expect($scope.saveAction).toEqual('Save');
+            expect($scope.submitted).toBe(false);
+        })
+    });
 
     describe('save article', function () {
         it('should call save on ArticleResource', function () {
-            articleResourceStub.save.returns({then: function () {
-                //TODO use sinon-as-promised to return a promise
-            }});
-            scope.article = {};
-            scope.save({$valid: true});
-            expect(articleResourceStub.save.calledWith(scope.article)).toBe(true);
+            $scope.save({$valid: true});
+            deferred.resolve({_id: 1, title: 'test title 1', content: 'test 1 content'});
+            $rootScope.$apply();
+            expect(articleResourceStub.save.called).toBe(true);
+            expect($scope.article.title).toEqual('test title 1');
+            expect($scope.saveAction).toEqual('Update');
         })
     });
-
-    describe('new article', function () {
-        it('should initialize a new article nad reset saveAction, submitted on the scope', function () {
-            scope.article = {title: 'testTitle'};
-            scope.initialize();
-            expect(scope.saveAction).toEqual('Save');
-            expect(scope.submitted).toBe(false);
-
-        })
-    });
-
 });
 
 describe('Controller : ArticleDisplayCtrl', function () {
 
-    var articleResourceStub, scope;
+    var articleResourceStub,
+        $q,
+        $rootScope,
+        $scope,
+        deferred;
 
     beforeEach(module('evtrsScrollApp'));
 
     beforeEach(function () {
-
         articleResourceStub = sinon.stub({'getById': function () {
         }});
-
         module(function ($provide) {
             $provide.value('ArticleResource', articleResourceStub);
         });
     });
 
-    beforeEach(function () {
-        articleResourceStub.getById.returns({then: function () {
-            //TODO use sinon-as-promised to return a promise
-        }});
-
-        module(function ($provide) {
-            $provide.value('ArticleResource', articleResourceStub);
-        });
-    });
-
-    beforeEach(inject(function ($controller, $rootScope, $q) {
-        scope = $rootScope.$new();
-        $controller('ArticleDisplayCtrl', {$scope: scope, $stateParams: {articleId: '1'}});
+    beforeEach(inject(function ($controller, _$rootScope_, _$q_) {
+        $rootScope = _$rootScope_;
+        $q = _$q_;
+        deferred = $q.defer();
+        $scope = $rootScope.$new();
+        articleResourceStub.getById.returns(deferred.promise);
+        $controller('ArticleDisplayCtrl', {$scope: $scope, $stateParams: {articleId: '2'}});
     }));
 
     describe('load article', function () {
         it('should call getById on ArticleResource', function () {
-            expect(articleResourceStub.getById.calledWith('1')).toBe(true);
+            deferred.resolve({_id: 2, title: 'test title 2', content: 'test 2 content'});
+            $rootScope.$apply();
+            expect(articleResourceStub.getById.calledWith('2')).toBe(true);
+            expect($scope.article.title).toEqual('test title 2');
+            expect($scope.article.content).toEqual('test 2 content');
         })
     });
 
