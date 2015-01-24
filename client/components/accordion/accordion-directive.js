8935520694c2303d2923e4d8d54c57bd01fa0da8
Peter Symoens <peter.symoens@eventures.io>
2015-01-24 17:10:53 +0100

    add background parallax

--- client/components/accordion/accordion-directive.js
+++ client/components/accordion/accordion-directive.js
@@ -5,24 +5,29 @@ angular.module('evtrsScrollApp').directive('accordion', function () {
     return {
         restrict: 'E',
         templateUrl: 'components/accordion/accordion.html',
+        allowMultipleOpen: '@',
         scope: {
             ngModel: '='
         },
         link: function (scope, element, attrs) {
+            var toggleCollapse = attrs.allowMultipleOpen;
             scope.panelBaseId = attrs.collapsePanelBodyId;
             scope.panelId = attrs.collapsePanelId;
+            //TODO rename
+            scope.toggleCollapsedStates = function (index) {
 
+                var contentEl = element.find('#' + scope.panelBaseId + '-' + index);
+                var titleEl = element.find('#' + scope.panelBaseId + '-title' + index);
 
-            scope.toggleCollapsedStates = function(ind){
-
-                angular.forEach(scope.ngModel, function(value, key){
-                    if (key == ind)
-                    {
-                        var contentEl = element.find('#' + scope.panelBaseId + '-' + ind);
-                        var titleEl = element.find('#' + scope.panelBaseId + '-title' + ind);
+                //TODO rework. iterate over view elements, and remove collapsed property from model
+                //rename ngModel , panelBase
+                angular.forEach(scope.ngModel, function (value, key) {
+                    if (key == index) {
+                        var contentEl = element.find('#' + scope.panelBaseId + '-' + index);
+                        var titleEl = element.find('#' + scope.panelBaseId + '-title' + index);
                         titleEl.toggleClass('accordionTitleActive');
                         contentEl.toggleClass('accordionItemCollapsed');
-                        if(scope.ngModel[key].collapsed){
+                        if (scope.ngModel[key].collapsed) {
                             contentEl.addClass('animateOut');
                             contentEl.removeClass('animateIn');
                         } else {
@@ -32,7 +37,9 @@ angular.module('evtrsScrollApp').directive('accordion', function () {
                         scope.ngModel[key].collapsed = !scope.ngModel[key].collapsed;
                     }
                     else {
-                     //   scope.ngModel[key].collapsed = false;
+//                        if (toggleCollapse !== true) {
+//                            scope.ngModel[key].collapsed = true;
+//                        }
                     }
                 });
             }
